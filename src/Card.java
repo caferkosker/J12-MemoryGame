4f4057bf5b0c84692483df7123abfb8aea83d50a
Caferkosker <caferkoskerr@gmail.com>
2024-01-30 22:57:59 +0300

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,24 @@
+public class Card {
+    private char value;
+    private boolean prediction = false;
+
+    public Card(char value) {
+        this.setValue(value);
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+    public void setValue(char value) {
+        this.value = value;
+    }
+
+    public boolean isPrediction() {
+        return prediction;
+    }
+
+    public void setPrediction(boolean prediction) {
+        this.prediction = prediction;
+    }
+}
